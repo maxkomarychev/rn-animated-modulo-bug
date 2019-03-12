31c73325caf2f6a778ba322045e3824bd426ec18
Max Komarychev <maxkomarychev@gmail.com>
2019-03-12 22:49:42 +0200

    Working example

--- App.js
+++ App.js
@@ -6,24 +6,70 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import { Animated, StyleSheet, Text, View, Button } from "react-native";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const FROM_VALUE = -1000;
+const TO_VALUE = 1000;
 
 type Props = {};
 export default class App extends Component<Props> {
+  state = {
+    animatedJS: new Animated.Value(0),
+    animatedNative: new Animated.Value(0)
+  };
+  runJS = () => {
+    this.state.animatedJS.setValue(FROM_VALUE);
+    Animated.timing(this.state.animatedJS, {
+      toValue: TO_VALUE,
+      duration: 3000
+    }).start();
+  };
+  runNative = () => {
+    this.state.animatedNative.setValue(FROM_VALUE);
+    Animated.timing(this.state.animatedNative, {
+      toValue: TO_VALUE,
+      duration: 3000,
+      useNativeDriver: true
+    }).start();
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={styles.column}>
+          <Text style={styles.text}>JS driver</Text>
+          <Button title="Run" onPress={this.runJS} />
+          <Animated.View
+            style={{
+              position: "absolute",
+              top: 200,
+              left: 5,
+              right: 5,
+              height: 50,
+              backgroundColor: "red",
+              transform: [
+                { translateY: Animated.modulo(this.state.animatedJS, 100) }
+              ]
+            }}
+          />
+        </View>
+        <View style={styles.column}>
+          <Text style={styles.text}>Native driver</Text>
+          <Button title="Run" onPress={this.runNative} />
+          <Animated.View
+            style={{
+              position: "absolute",
+              top: 200,
+              left: 5,
+              right: 5,
+              height: 50,
+              backgroundColor: "green",
+              transform: [
+                { translateY: Animated.modulo(this.state.animatedNative, 100) }
+              ]
+            }}
+          />
+        </View>
       </View>
     );
   }
@@ -31,19 +77,20 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: "row",
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
-  welcome: {
+  column: {
+    marginTop: 50,
+    flex: 1,
+    height: "100%"
+  },
+  text: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    margin: 10
+  }
 });
